3cf090c15ae1b1d6cb6b9d0f7695e79d37351ba0
iwamura-saaya <82559952+iwamura-saaya@users.noreply.github.com>
2023-05-10 20:32:55 +0900

    Add files via upload

---
+++ Fizzbuzz.js
@@ -0,0 +1,12 @@
+for(i=1;i<=20;i++){
+     if(i%3 == 0 && i%5 == 0){
+         document.write('FizzBuzz</br>'.bold().fontsize(4));
+     }else if(i%3 == 0){
+         document.write('Fizz</br>'.bold().fontsize(4));
+     }else if(i%5 == 0){
+         document.write('Buzz</br>'.bold().fontsize(4));
+     }else{
+         document.write(i+'</br>');
+     }
+}
+
